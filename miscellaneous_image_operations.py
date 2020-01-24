a167a98ea89e887c597d4c3222675825e337653d
Rignak <aureliencolin@hotmail.com>
2020-01-24 14:27:00 +0100

    Add function to make the fourier transform and inverse it.

--- miscellaneous_image_operations.py
+++ miscellaneous_image_operations.py
@@ -5,6 +5,7 @@ import os
 import cv2
 import tqdm
 
+
 def extract_checked_bound(im, x_min, x_max, y_min, y_max):
     x_min, y_min = check_bound(im, x_min, y_min)
     x_max, y_max = check_bound(im, x_max, y_max)
@@ -26,32 +27,34 @@ def check_bound(im, x, y):
 def median_subsampling(block):
     return np.median(block, axis=[2, 3])
 
+
 def square_image(im):
     dim = np.max(im.shape[:2])
     if len(im.shape) == 3:
-        square_im = np.zeros((dim, dim,3))
+        square_im = np.zeros((dim, dim, 3))
     else:
         square_im = np.zeros((dim, dim))
 
-    offset_x = (dim-im.shape[0])//2
-    offset_y = (dim-im.shape[1])//2
+    offset_x = (dim - im.shape[0]) // 2
+    offset_y = (dim - im.shape[1]) // 2
     if offset_x != 0:
-        square_im[offset_x:offset_x+im.shape[0]] = im
+        square_im[offset_x:offset_x + im.shape[0]] = im
     elif offset_y != 0:
-        square_im[:, offset_y:offset_y+im.shape[1]] = im
+        square_im[:, offset_y:offset_y + im.shape[1]] = im
     else:
         square_im = im
     return square_im
-    
-def main(input_folder, output_folder, shape, 
+
+
+def main(input_folder, output_folder, shape,
          authorized_extension=('.png', '.jpg', '.jpeg')):
     for filename in tqdm.tqdm(os.listdir(input_folder)):
         split_path = os.path.splitext(filename)
         if not split_path[1] or split_path[1] not in authorized_extension:
             continue
         path = os.path.join(input_folder, filename)
-        end_path = os.path.join(output_folder, split_path[0]+'.png')
-        
+        end_path = os.path.join(output_folder, split_path[0] + '.png')
+
         im = cv2.imread(path)
         if im is None:
             print(f"{path} is invalid")
@@ -60,6 +63,27 @@ def main(input_folder, output_folder, shape,
 
         im = imutils.resize(im, width=shape[0], height=shape[1], inter=cv2.INTER_CUBIC)
         cv2.imwrite(end_path, im)
-    
+
+
+def fourier_transform(image, remove_center=False):
+    ftimage = np.fft.fft2(image)
+    ftimage = np.fft.fftshift(ftimage)
+
+    if remove_center:
+        ftimage[image.shape[0] // 2] = 0
+        ftimage[:, image.shape[1] // 2] = 0
+
+    return ftimage, np.abs(np.real(ftimage)), np.abs(np.imag(ftimage))
+
+
+def inverse_fourier_transform(ftimage, m=0):
+    if ftimage.shape[-1] == 2:
+        ftimage = ftimage[:, :, 0] + ftimage[:, :, 1] * 1j
+
+    ftimage = np.fft.fftshift(ftimage)
+    imagep = np.fft.ifft2(ftimage) + m
+    return imagep.astype(np.float64)
+
+
 if __name__ == '__main__':
     fire.Fire(main)
